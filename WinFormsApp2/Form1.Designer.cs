0436421b7a5a3e2ae6b15f3601b6362be2c6f275
decerasyon <muhammedemin33@gmail.com>
2022-06-15 16:32:55 +0300

    Smith waterman algorithm
    Smith waterman algorithm

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.matchBox = new System.Windows.Forms.TextBox();
+            this.mismatchBox = new System.Windows.Forms.TextBox();
+            this.gapBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.yol = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Baz 1 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Baz 2 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(736, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "match :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(736, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "mismatch :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(736, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "gap :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(736, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 15);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Çalışma Süresi : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(736, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 15);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Puan : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // matchBox
+            // 
+            this.matchBox.Location = new System.Drawing.Point(808, 23);
+            this.matchBox.Name = "matchBox";
+            this.matchBox.Size = new System.Drawing.Size(100, 23);
+            this.matchBox.TabIndex = 4;
+            // 
+            // mismatchBox
+            // 
+            this.mismatchBox.Location = new System.Drawing.Point(808, 56);
+            this.mismatchBox.Name = "mismatchBox";
+            this.mismatchBox.Size = new System.Drawing.Size(100, 23);
+            this.mismatchBox.TabIndex = 5;
+            // 
+            // gapBox
+            // 
+            this.gapBox.Location = new System.Drawing.Point(808, 84);
+            this.gapBox.Name = "gapBox";
+            this.gapBox.Size = new System.Drawing.Size(100, 23);
+            this.gapBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 72);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 210);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(981, 361);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(736, 173);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Yol : ";
+            this.label8.Click += new System.EventHandler(this.yolLabel_Click);
+            // 
+            // yol
+            // 
+            this.yol.AutoSize = true;
+            this.yol.Location = new System.Drawing.Point(793, 173);
+            this.yol.Name = "yol";
+            this.yol.Size = new System.Drawing.Size(0, 15);
+            this.yol.TabIndex = 8;
+            this.yol.Click += new System.EventHandler(this.yolLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 583);
+            this.Controls.Add(this.yol);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gapBox);
+            this.Controls.Add(this.mismatchBox);
+            this.Controls.Add(this.matchBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox matchBox;
+        private System.Windows.Forms.TextBox mismatchBox;
+        private System.Windows.Forms.TextBox gapBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label yol;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //default match mismatch ve gap values..
+            matchBox.Text = "1";
+            mismatchBox.Text = "-1";
+            gapBox.Text = "-2";
+        }
+        public static string TersCevir(string metin)
+        {
+            string sonuc = "";
+            for (int i = metin.Length - 1; i >= 0; i--)
+                sonuc += metin[i];
+            return sonuc;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime then = DateTime.Now;
+            dataGridView1.Rows.Clear();
+            //Read file
+            StreamReader SR = new StreamReader("C:\\Users\\MEaym\\Desktop\\LibrarySolution\\WinFormsApp2\\seq1.txt");
+            textBox1.Text = SR.ReadLine();
+            SR.Close();
+
+            StreamReader SR2 = new StreamReader("C:\\Users\\MEaym\\Desktop\\LibrarySolution\\WinFormsApp2\\seq2.txt");
+            textBox2.Text = SR2.ReadLine();
+            SR2.Close();
+            //
+
+            string baz1;
+            baz1 = Convert.ToString(textBox1.Text);
+            string baz2;
+            baz2 = Convert.ToString(textBox2.Text);
+            dataGridView1.RowCount = baz1.Length + 2;
+            dataGridView1.ColumnCount = baz2.Length + 2;
+            dataGridView1.ColumnHeadersVisible = false;
+
+
+            //placing bases in rows and columns
+            for (int i = 2; i < baz1.Length + 2; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = Convert.ToString(baz1[i - 2]);
+
+            }
+            for (int i = 2; i < baz2.Length + 2; i++)
+            {
+                dataGridView1.Rows[0].Cells[i].Value = Convert.ToString(baz2[i - 2]);
+            }//
+
+
+            int match = Convert.ToInt32(matchBox.Text);
+            int mismatch = Convert.ToInt32(mismatchBox.Text);
+            int gap = Convert.ToInt32(gapBox.Text);
+
+            dataGridView1.Rows[1].Cells[1].Value = 0;
+            //Adding gap values
+            for (int i = 2; i < baz2.Length + 2; i++)
+            {
+                dataGridView1.Rows[1].Cells[i].Value = Convert.ToInt32(dataGridView1.Rows[1].Cells[i - 1].Value) + gap;
+                int kontrol = Convert.ToInt32(dataGridView1.Rows[1].Cells[i].Value) + gap;
+                if (kontrol < 0)
+                {
+                    dataGridView1.Rows[1].Cells[i].Value = 0;
+                }
+            }
+            for (int i = 2; i < baz1.Length + 2; i++)
+            {
+                dataGridView1.Rows[i].Cells[1].Value = Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[1].Value) + gap;
+                int kontrol = Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[1].Value) + gap;
+                if (kontrol < 0)
+                {
+                    dataGridView1.Rows[i].Cells[1].Value = 0;
+                }
+            }
+            int ustdeger;
+            int soldeger;
+            int solust;
+
+            for (int i = 2; i < baz1.Length + 2; i++)//row
+            {
+                for (int j = 2; j < baz2.Length + 2; j++)//column
+                {
+                    ustdeger = Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[j].Value) + gap;
+                    soldeger = Convert.ToInt32(dataGridView1.Rows[i].Cells[j - 1].Value) + gap;
+
+                    if (Convert.ToString(dataGridView1.Rows[i].Cells[0].Value) != Convert.ToString(dataGridView1.Rows[0].Cells[j].Value))//if bases diff
+                    {
+                        solust = Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[j - 1].Value) + mismatch;
+
+                        if (soldeger >= ustdeger && soldeger >= solust)//if left bigger
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = soldeger;
+                            if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) <= 0)
+                            {
+                                dataGridView1.Rows[i].Cells[j].Value = 0;
+                            }
+                        }
+                        else if (ustdeger >= soldeger && ustdeger >= solust)//if top bigger
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = ustdeger;
+                            if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) <= 0)
+                            {
+                                dataGridView1.Rows[i].Cells[j].Value = 0;
+                            }
+
+                        }
+                        else if (solust >= ustdeger && solust >= soldeger)//if topleft bigger
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = solust;
+                            if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) <= 0)
+                            {
+                                dataGridView1.Rows[i].Cells[j].Value = 0;
+                            }
+
+                        }
+                    }
+                    else //if bases same
+                    {
+                        solust = Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[j - 1].Value) + match;
+                        if (soldeger >= ustdeger && soldeger >= solust)//left values bigger
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = soldeger;
+                            if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) <= 0)
+                            {
+                                dataGridView1.Rows[i].Cells[j].Value = 0;
+                            }
+
+                        }
+                        else if (ustdeger >= soldeger && ustdeger >= solust)//top values bigger
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = ustdeger;
+                            if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) <= 0)
+                            {
+                                dataGridView1.Rows[i].Cells[j].Value = 0;
+                            }
+
+                        }
+                        else if (solust >= ustdeger && solust >= soldeger)//topleft bigger
+                        {
+                            dataGridView1.Rows[i].Cells[j].Value = solust;
+                            if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) <= 0)
+                            {
+                                dataGridView1.Rows[i].Cells[j].Value = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            int enBuyuk = 0;
+            int newI = 0;
+            int newJ = 0;
+            for (int i = 2; i < baz1.Length + 2; i++)
+            {
+                for (int j = 2; j < baz2.Length + 2; j++)
+                {
+                    if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) >= enBuyuk)
+                    {
+                        enBuyuk = Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                        newI = i;
+                        newJ = j;
+                    }
+                }
+            }
+            int puan = Convert.ToInt32(dataGridView1.Rows[newI].Cells[newJ].Value);
+            label7.Text = "Puan : " + puan;
+
+            string text1 = "";
+            string text2 = "";
+
+            dataGridView1.Rows[newI].Cells[newJ].Style.BackColor = Color.LightGreen;
+
+            for (int j = newJ, i = newI; j > 1 || i > 1;)
+            {
+                if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) != 0)
+                {
+                    if (i == 1)
+                    {
+                        dataGridView1.Rows[i].Cells[j - 1].Style.BackColor = Color.LightGreen;
+                        j--;
+                    }
+                    else if (j == 1)
+                    {
+                        dataGridView1.Rows[i - 1].Cells[j].Style.BackColor = Color.LightGreen;
+                        i--;
+                    }
+                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j - 1].Value) + gap >= Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value))
+                    {
+                        dataGridView1.Rows[i].Cells[j - 1].Style.BackColor = Color.LightGreen;
+                        text1 += "-";
+                        text2 += Convert.ToString(dataGridView1.Rows[i].Cells[0].Value); j--;
+                    }
+                    else if (Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[j].Value) + gap >= Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value))
+                    {
+                        dataGridView1.Rows[i - 1].Cells[j].Style.BackColor = Color.LightGreen;
+                        text1 += Convert.ToString(dataGridView1.Rows[0].Cells[j].Value);
+                        text2 += "-"; i--;
+                    }
+                    else if (Convert.ToInt32(dataGridView1.Rows[i - 1].Cells[j - 1].Value) + gap >= Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value))
+                    {
+                        dataGridView1.Rows[i - 1].Cells[j - 1].Style.BackColor = Color.LightGreen;
+                        text1 += Convert.ToString(dataGridView1.Rows[0].Cells[j - 1].Value);
+                        text2 += Convert.ToString(dataGridView1.Rows[i - 1].Cells[0].Value); i--; j--;
+                    }
+                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) != 0)
+                    {
+                        dataGridView1.Rows[i - 1].Cells[j - 1].Style.BackColor = Color.LightGreen;
+                        text1 += Convert.ToString(dataGridView1.Rows[0].Cells[j - 1].Value);
+                        text2 += Convert.ToString(dataGridView1.Rows[i - 1].Cells[0].Value);
+                        i--; j--;
+                    }
+                }
+                else
+                {
+
+                    dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.LightGreen;
+                    break;
+                }
+            }
+            yol.Text = TersCevir(text1) + "\n" + TersCevir(text2);
+
+            /////////////////////////operation time
+            DateTime now = DateTime.Now;
+            label6.Text = "Çalışma Süresi: " + (now.Millisecond - then.Millisecond) + "ms";
+
+        }
+
+        private void yolLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
